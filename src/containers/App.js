c1a3c3be56d68ff3e0dbd603f5e8c549d6c09fa7
Easonwang01 <jason40115@hotmail.com>
2016-10-24 21:32:01 +0800

    add

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react'
 import TodoList from './TodoList.js'
 import {connect} from 'react-redux'
+import Nav from './Nav.js'
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <h1>Todo list</h1>
+        <Nav />
+        <div style={style.content}>
+        <h1>Tode li</h1>
         {this.props.children}
+        </div>
       </div>
     )
   }
 
 }
+const style = {
+  content: {
+    marginTop: "5%",
+    marginLeft: "5%"
+  }
+}
+
 function  mapStateToProp(state){
 
 	return state
