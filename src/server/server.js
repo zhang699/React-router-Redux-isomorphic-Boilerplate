95bd424582423377614131939ed759843317bff7
Easonwang01 <jason40115@hotmail.com>
2016-10-25 12:34:54 +0800

    'add'

--- src/server/server.js
+++ src/server/server.js
@@ -64,8 +64,9 @@ const renderFullPage = (html, preloadedState) => (`
   <meta charset="UTF-8">
   <title>React Todo List</title>
 	<link rel="stylesheet" href="../materialize.min.css">
-	<script src="../jquery.min.js""></script>
-  <script src="../materialize.min.js"></script>
+	<script  src="../jquery.min.js""></script>
+<script src="../materialize.min.js"></script>
+
 </head>
 <body>
   <div id="app">${html}</div>
