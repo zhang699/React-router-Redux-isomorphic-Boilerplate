7944386f018f6e54a5ec3b4f0596e82bee478229
EasonWang01 <jason40115@hotmail.com>
2016-11-18 16:23:15 +0800

    code split ioServer

---
+++ src/server/ioServer.js
@@ -0,0 +1,21 @@
+export default ioServer = () => {
+
+  io.on('connection', function(socket){
+
+    console.log('a user connected');
+    //發表文章
+  	socket.on('postArticle', function(){
+  		post({
+  			host: 'localhost',
+  			port: '3001',
+  			path: '/getArticle'
+  		},'hi')
+  		.then(function(data){
+  			socket.broadcast.emit('updateArticle',JSON.parse(data));//broadcast傳給所有人除了自己
+  			socket.emit('updateArticle',JSON.parse(data));//加上傳給自己的socket
+  		});
+    });
+
+
+  });
+}
