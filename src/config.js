4633655dcbad93271d6d1c9b9e67f37b539b2bcd
yicheng <jason40115@hotmail.com>
2016-12-16 15:33:12 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   email: 'jason71248@gmail.com',
-  pass: 'jason2911',
+  pass: '',
   dbURL: 'mongodb://forclass1:test123@ds013898.mlab.com:13898/forclass',
   jwtSecret: 'yicheng',
   origin: 'http://localhost:3001'
