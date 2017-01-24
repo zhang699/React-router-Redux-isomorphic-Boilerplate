140d96a00801ca9454f51e9060599f52b40634ea
Jimmy.Jhang <t6820310@ntut.org.tw>
2017-01-24 12:08:26 +0800

    Export config for server.js to access

--- src/config.js
+++ src/config.js
@@ -2,8 +2,9 @@
 const config = {// fill in yourself info below
   email: '',
   pass: '',
-  dbURL: '',
+  dbURL: '',//example:mongodb://localhost:27017/
   jwtSecret: '',
   origin: 'http://localhost:3001'
 }
 
+export default config;
